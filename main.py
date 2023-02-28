4ac8f90f7ed20b2b3b7b005ef3dfc12b1011c71c
Juan Marroquin <65145980+Juan520dlc@users.noreply.github.com>
2023-02-28 10:24:33 -0600

    Subida y le[ida de ruta de archivo
    Pendiente de interfaz grafica y que muestre el texto del archivo en la interfaz grafica.

--- main.py
+++ main.py
@@ -1,5 +1,61 @@
-import lex
-import yacc
+# Limbreria lexica y sintactica
+import ply.lex as lex
+import ply.yacc as yacc
+import tkinter as tk
+from tkinter import filedialog
+
+# Crear la ventana principal
+ventana = tk.Tk()
+
+# Función para manejar el botón "Seleccionar archivo"
+def seleccionar_archivo():
+    # Abrir un cuadro de diálogo para seleccionar un archivo
+    archivo = filedialog.askopenfile()
+    # Obtener la ruta del archivo seleccionado
+    ruta_archivo = archivo.name
+    # Imprimir la ruta en la consola (opcional)
+    print("Archivo seleccionado:", ruta_archivo)
+
+# Crear el botón "Seleccionar archivo"
+boton_seleccionar = tk.Button(ventana, text="Seleccionar archivo", command=seleccionar_archivo)
+boton_seleccionar.pack()
+
+# Mostrar la ventana principal
+ventana.mainloop()
+
+
+
+# Definir las reglas de análisis léxico
+tokens = ('NUMERO', 'MAS')
+
+def t_NUMERO(token):
+    r'\d+'
+    token.value = int(token.value)
+    return token
+
+def t_MAS(token):
+    r'\+'
+    return token
+
+t_ignore = ' \t\n'
+
+# Definir las reglas de análisis sintáctico
+def p_suma(p):
+    'expresion : NUMERO MAS NUMERO'
+    p[0] = p[1] + p[3]
+
+# Crear el analizador léxico y sintáctico
+analizador_lexico = lex.lex()
+analizador_sintactico = yacc.yacc()
+
+# Leer la expresión a compilar
+expresion = input("Ingrese una expresión matemática simple: ")
+
+# Analizar la expresión
+resultado = analizador_sintactico.parse(expresion, lexer=analizador_lexico)
+
+# Mostrar el resultado
+print("El resultado de la expresión es:", resultado)
 
 # Palabras reservadas
 P_reservada = {
